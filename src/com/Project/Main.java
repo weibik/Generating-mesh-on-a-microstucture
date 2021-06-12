445c8210926e815f19fcec651900a0cddce09948
weibik <j-szkola@wp.pl>
2021-06-12 12:03:02 +0200

    Basics of the algorithm

--- src/com/Project/Main.java
+++ src/com/Project/Main.java
@@ -1,8 +1,23 @@
 package com.Project;
 
+/*
+
+Quad tree -- Jakub Szkoła
+
+ */
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Task.doTask();
     }
 }
---
+++ src/com/Project/Task.java
@@ -0,0 +1,39 @@
+package com.Project;
+
+
+//"/resources/singleSlice.png"
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+class Task {
+
+    public static void doTask(){
+
+
+
+        /*
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(new File("src/resources/singleSlice.png"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        JLabel lbl = new JLabel();
+        lbl.setIcon(new ImageIcon(img));
+        frame.getContentPane().add(lbl, BorderLayout.CENTER);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+         */
+    }
+
+}
